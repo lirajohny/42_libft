11bbd5f34c3e63acbdc0e98fbbafc86dd2ff3f02
lirajohny <johnylira.dev@gmail.com>
2023-10-11 20:34:14 -0300

    update

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jlira <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 14:32:24 by jlira             #+#    #+#             */
-/*   Updated: 2023/10/11 20:19:19 by jlirasv          ###   ########.fr       */
+/*   Updated: 2023/10/11 20:30:08 by jlirasv          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,18 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	j;
 	size_t	k;
 
-	i = ft_strlen(dst);
-	j = ft_strlen(src);
-	k = 0;
-	while (src[k] != '\0' && (i + 1 < dstsize))
-	{
-		dst[i] = src[k];
+	i = 0;
+	while (dst[i] != '\0' && i < dstsize)
 		i++;
-		k++;
+	k = i;
+	j = 0;
+	while (src[j] != '\0' && (i + 1 < dstsize))
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
 	}
-	if (i < dstsize)
+	if (k < dstsize)
 		dst[i] = '\0';
-	return (i + j);
+	return (ft_strlen(src) + k);
 }
