af88a6431714b07bc74df80c7625b011a53e9b42
Johny Lira <johnylira.dev@gmail.com>
2023-10-26 14:10:24 -0300

    update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jlira <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 11:00:51 by jlira             #+#    #+#             */
-/*   Updated: 2023/10/11 19:44:12 by jlirasv          ###   ########.fr       */
+/*   Updated: 2023/10/26 14:09:46 by jlira            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned char	*temp_src;
 
 	i = 0;
+	if (!src && !dst)
+		return (NULL);
 	temp_dst = (unsigned char *)dst;
 	temp_src = (unsigned char *)src;
 	while (i < n)
